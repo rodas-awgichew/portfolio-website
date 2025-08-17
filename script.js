6cb841889fcb18ddfa6ced25936d9428496a1c6d
Sadorra <rodiawg@gmail.com>
2025-08-17 22:42:06 +0300

    code updated.

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
- const hamburger = document.getElementById('hamburger');
-  const navMenu = document.getElementById('navMenu');
+const hamburger = document.getElementById('hamburger');
+const navMenu = document.getElementById('navMenu');
+const navLinks = document.querySelectorAll("#navMenu a");
 
-  hamburger.addEventListener('click', () => {
+hamburger.addEventListener('click', () => {
     navMenu.classList.toggle('active');
   });
+
+ 
+navLinks.forEach(link => {
+  link.addEventListener("click", () => {
+    navMenu.classList.remove("active");
+  });
+});
+
+
+
